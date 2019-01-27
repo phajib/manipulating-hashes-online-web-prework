26390b315e2b67f46c735f47b7b6474e9084e83b
H B <phajib@gmail.com>
2019-01-27 09:30:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,13 +6,13 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  new_array = []
-  groceries.values do |k,v|
-    new_array<<v
-    new_array.flatten
-    new_array
-  end
+  groceries.values.flatten
+  #new_array = []
+  #groceries.values do |k,v|
+  #  new_array<<v
+  #  new_array.flatten
+  #  new_array
+  #end
   #binding.pry
   #new_array
 end
