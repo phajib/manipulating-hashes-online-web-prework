fec9598eb3937beea38336885506d0238e7ce7be
H B <phajib@gmail.com>
2019-01-27 09:04:12 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,7 +8,7 @@ def second_challenge
   new_array = []
   groceries.values do |k,v|
     new_array << v
-    new_array.flatten(
+    new_array.flatten
     #v.flatten
   end
   #code your solution here!
