0190129c081a213bb47fe2ebbb37b2ab8698073b
H B <phajib@gmail.com>
2019-01-27 09:03:25 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,11 +5,11 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-  #new_array = []
+  new_array = []
   groceries.values do |k,v|
-    #new_array << v
+    new_array << v
     #new_array.flatten(1)
-    v.flatten
+    #v.flatten
   end
   #code your solution here!
 
